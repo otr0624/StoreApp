dd8823f90cf2c3661dc08c8dbbbcef5b09e0e261
otr0624 <otr0624@mail.com>
2020-04-14 17:51:23 -0400

    Minor refactoring improvements (lecture 8)

--- resources/item.py
+++ resources/item.py
@@ -1,10 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import (
-    get_jwt_claims,
-    jwt_required,
-    jwt_optional,
-    get_jwt_identity,
-    fresh_jwt_required)
+from flask_jwt_extended import jwt_required
 from models.item import ItemModel
 
 class Item(Resource):
@@ -27,7 +22,7 @@ class Item(Resource):
             return item.json()
         return {'message': 'Item not found'}, 404
 
-    @fresh_jwt_required
+    @jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
@@ -45,15 +40,13 @@ class Item(Resource):
 
     @jwt_required
     def delete(self, name):
-        claims = get_jwt_claims()
-        if not claims['is_admin']:
-            return {'message': 'Admin privilege required'}, 401
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
+            return {'message': 'Item deleted'}
+        return {'message': 'Item not found'}
 
-        return {'message': 'Item deleted'}
-
+    @jwt_required
     def put(self, name):
         data = Item.parser.parse_args()
 
@@ -70,13 +63,32 @@ class Item(Resource):
 
 
 class ItemList(Resource):
-    @jwt_optional
     def get(self):
-        user_id = get_jwt_identity()
-        items = [item.json() for item in ItemModel.find_all()]
-        if user_id:
-            return {'items': items}, 200
-        return {
-            'items': [item['name'] for item in items],
-            'message': 'More data available if you log in.'
-        }, 200
+        return {'items': [item.json() for item in ItemModel.find_all()]}, 200
+
+
+# ADMIN-ONLY VERSION OF DELETE ENDPOINT (IMPORT get_jwt_claims AT TOP):
+#     @jwt_required
+#     def delete(self, name):
+#         claims = get_jwt_claims()
+#         if not claims['is_admin']:
+#             return {'message': 'Admin privilege required'}, 401
+#         item = ItemModel.find_by_name(name)
+#         if item:
+#             item.delete_from_db()
+#
+#         return {'message': 'Item deleted'}
+
+#JWT-OPTIONAL VERSION OF ITEMS ENDPOINT (IMPORT jwt_optional, get_jwt_identity AT TOP):
+#
+# class ItemList(Resource):
+#     @jwt_optional
+#     def get(self):
+#         user_id = get_jwt_identity()
+#         items = [item.json() for item in ItemModel.find_all()]
+#         if user_id:
+#             return {'items': items}, 200
+#         return {
+#             'items': [item['name'] for item in items],
+#             'message': 'More data available if you log in.'
+#         }, 200
